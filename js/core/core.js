560dd3c1d4c17ace4ca78db9f92d875dbcf2ccb1
AndMarc-rpo251 <andrejmarcenuk40@gmail.com>
2026-03-18 12:37:08 +0300

    Update core.js

--- js/core/core.js
+++ js/core/core.js
@@ -1,7 +1,1 @@
-// Функция для рисования тела игрока (жёлтый круг)
-window.drawPlayerBody = function(ctx, x, y) {
-    // TODO: Жёлтый круг радиусом 15 в точке (x, y)
-    // Используй: fillStyle = 'yellow', beginPath, arc, fill
-    // 👇 Твой код здесь
-    
-}
+
