12fd7a28b925862688e6c57d4ed9710077961933
GEAR ENCORE <ila289193@gmail.com>
2026-03-18 12:43:48 +0300

    Update core.js
    Add playereyes

--- js/core.js
+++ js/core.js
@@ -5,3 +5,11 @@ window.drawPlayerBody = function(ctx, x, y) {
     // 👇 Твой код здесь
   console.log("Работает")
 }
+
+// Функция для рисования глаз игрока
+window.drawPlayerEyes = function(ctx, x, y) {
+    // TODO: Два белых круга радиусом 3 на (x-5, y-5) и (x+5, y-5)
+    // TODO: Два чёрных круга радиусом 1.5 внутри них
+    // 👇 Твой код здесь
+    
+}
